d9bca5cc30e323c928c702a4109eb9ce35d3ac8f
radvanall <rip777ton@gmail.com>
2021-04-20 19:21:55 +0300

    Initial commit. Added i main class and a readme.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   ex5.class
    	new file:   ex5.java
    	new file:   readme.txt

---
+++ ex5.java
@@ -0,0 +1,26 @@
+public class ex5 {
+
+	public static void main(String[] args) {
+
+	int a=27,b=15,d=0;
+	
+	int sumaAB=a+b;
+	int diferentaAB=a-b;
+	int produsulAB=a*b;
+
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+	System.out.println("sumaAB="+sumaAB);
+	System.out.println("diferentaAB="+diferentaAB);
+	System.out.println("produsulAB="+produsulAB);
+
+	System.out.println("d="+d);
+	d=d+1;
+	System.out.println("d="+d);
+	d+=1;
+	System.out.println("d="+d);
+	d++;
+	System.out.println("d="+d);
+ 
+     }
+}
